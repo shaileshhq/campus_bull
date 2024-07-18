6a52d8978384b6eeb861ca26f4b39ee90b13bb6b
ashutshm778 <ashutoshm778@gmail.com>
2024-07-18 14:51:46 +0530

    new update

---
+++ resources/views/frontend/pages/subscription-plans-pay.blade.php
@@ -0,0 +1,40 @@
+@extends('frontend.layouts.front_app')
+@section('content')
+    <!-- Page Banner Start -->
+    <div class="section page-banner-section bg-color-1">
+        <img class="shape-4" src="{{ asset('public/front_css/assets/images/shape/shape-21.png') }}" alt="shape">
+        <img class="shape-5" src="{{ asset('public/front_css/assets/images/shape/shape-21.png') }}" alt="shape">
+
+        <div class="container">
+            <!-- Page Banner Content Start -->
+            <div class="page-banner-content">
+                <h2 class="title">Plan Payment</h2>
+                <ul class="breadcrumb justify-content-center">
+                    <li class="breadcrumb-item"><a href="/">Home</a></li>
+                    <li class="breadcrumb-item active">Subsciption Plan Pay</li>
+                </ul>
+            </div>
+            <!-- Page Banner Content End -->
+        </div>
+    </div>
+    <!-- Page Banner End -->
+    <!-- Pricing Start -->
+    <div class="section mb-5">
+        <div class="container mb-5">
+            <!-- Pricing Wrapper End -->
+           
+            <!-- Pricing Wrapper End -->
+
+        </div>
+    </div>
+    <!-- Pricing End -->
+    <!-- The Modal -->
+  
+
+
+  
+@endsection
+
+@section('script')
+   
+@endsection
